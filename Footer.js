b81572ae482d0188f99f1834227509e5120b3a54
MoshinaT <55828439+MoshinaT@users.noreply.github.com>
2019-10-13 19:53:37 +0530

    Update Footer.js

--- Footer.js
+++ Footer.js
@@ -3,14 +3,13 @@ import React from "react";
 export default () => {
 	return (
 		<footer className="footer bg-dark text-white mt-5 p-4 text-center">
-			Made with <i className="fas fa-heart" /> and <i className="fas fa-code" />{" "}
-			by{" "}
+			
 			<a
-				href="https://geekysrm.github.io"
+				href="https://MoshinaT.github.io"
 				target="_blank"
 				rel="noopener noreferrer"
 			>
-				Soumya Ranjan Mohanty
+				Moshina Thajudeen
 			</a>
 		</footer>
 	);
